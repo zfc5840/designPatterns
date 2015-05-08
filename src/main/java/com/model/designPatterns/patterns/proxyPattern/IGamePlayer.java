7f31452807286639884ea5bef9d418f9b688d024
zfc5840 <1076231986@qq.com>
2015-05-08 15:48:06 +0800

    添加代理模式

---
+++ src/main/java/com/model/designPatterns/patterns/proxyPattern/IGamePlayer.java
@@ -0,0 +1,15 @@
+/**
+ * Description: IGamePlayer.java
+ * All Rights Reserved.
+ * @version 1.0  2015-5-8 下午2:30:29  by 张富成（fc.zhang@zuche.com）创建
+ */
+package com.model.designPatterns.patterns.proxyPattern;
+
+/**
+ * Description: 
+ * All Rights Reserved.
+ * @version 1.0  2015-5-8 下午2:30:29  by 张富成（fc.zhang@zuche.com）创建
+ */
+public interface IGamePlayer {
+
+}
